d3acf90a2a1167b65d52c1a998e81b6bff10bb67
斎藤 岳 <gaku@saitou-no-MacBook-Pro.local>
2016-10-22 16:41:04 +0900

    [パサラン]unityプロジェクト作成

---
+++ New Unity Project/Assets/camera_moved.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera_moved : MonoBehaviour {
+	public Transform moveCam;
+	public float smoothing = 0.5f;
+	Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = moveCam.transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnBecameInvisible(){
+		Debug.Log(offset.z);
+		Vector3 newPosition = moveCam.transform.position;
+		newPosition.x = transform.position.x + offset.x;
+		newPosition.y = transform.position.y + offset.y;
+		newPosition.z = transform.position.z + offset.z;
+		moveCam.transform.position = Vector3.Lerp(newPosition,transform.position, smoothing * Time.deltaTime);
+	}
+}
